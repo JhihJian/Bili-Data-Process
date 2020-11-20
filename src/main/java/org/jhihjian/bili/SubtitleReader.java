e72aeec8c25f46b85112059342a17271e9326d99
JhihJian <980858153@qq.com>
2020-11-20 12:44:06 +0800

    改为csv读取,补充单元测试
    Signed-off-by: JhihJian <980858153@qq.com>

--- src/main/java/org/jhihjian/bili/SubtitleReader.java
+++ src/main/java/org/jhihjian/bili/SubtitleReader.java
@@ -2,6 +2,8 @@ package org.jhihjian.bili;
 
 import com.google.common.base.Joiner;
 import com.google.common.base.Strings;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,39 +18,24 @@ public class SubtitleReader {
     private final Logger logger= LoggerFactory.getLogger(this.getClass().getName());
     public String getTotalText(String filePath) throws IOException {
         logger.info("extract text from {}",filePath);
-        String content=Files.readString(Paths.get(filePath), StandardCharsets.UTF_8);
-        content=content.replace("\"","");
-        List<String> result= Arrays.stream(content.split("\n")).filter(s->!Strings.isNullOrEmpty(s)).filter(s->!s.equals("\r")&&!s.equals("\n")).collect(Collectors.toList());
-        //result:{"42960,钱包都快跪下来求我当人了"}
+        FileReader fileReader=new FileReader(filePath);
         Map<Long,String> timeTextMap=new TreeMap<>();
         String preText="";
-        for(String line:result){
+        for (CSVRecord line : CSVFormat.DEFAULT.parse(fileReader)) {
             Long timestamp;
             String text;
             try {
-                int index=line.indexOf(',');
-                if(index<0){
-                    logger.error("not found splitter,line:{}",line);
-                    continue;
-                }
-                 timestamp=Long.parseLong( line.substring(0,index));
-                 text=line.substring(index+1);
-            }
-            catch (Exception e){
+                timestamp = Long.parseLong(line.get(0));
+                text=line.get(1);
+            }catch (Exception e){
                 logger.error("timestamp parse error,line:{}",line);
                 continue;
             }
-            if( Strings.isNullOrEmpty(text)||"\r".equals(text)){
-                continue;
-            }
-            //去重
-            if(!preText.equals(text)){
-                //"www.thelancet.com Vol391 June 2"
+            if(!Strings.isNullOrEmpty(text)&&!preText.equals(text)){
                 timeTextMap.put(timestamp, text);
             }
             preText=text;
         }
-        System.out.println(Joiner.on(",").join( timeTextMap.values()));
         return Joiner.on(",").join( timeTextMap.values());
     }
 }
--- src/test/java/org/jhihjian/bili/SubtitleReaderTest.java
+++ src/test/java/org/jhihjian/bili/SubtitleReaderTest.java
@@ -1,5 +1,6 @@
 package org.jhihjian.bili;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,6 +17,6 @@ public class SubtitleReaderTest {
     @Test
     public void getTotalText() throws IOException {
         SubtitleReader subtitleReader=new SubtitleReader();
-        subtitleReader.getTotalText(path);
+        Assert.assertEquals( "钱包都快跪下来求我当人了,民族荣誉招你煮你了,这是一个有底线的企业,但凡是工业品,每一个环节都存在风险",subtitleReader.getTotalText(path));
     }
 }
